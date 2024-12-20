53a6b8cbd501224ad2fe3dcfefd2b995db83c2aa
gift95 <55743744+gift95@users.noreply.github.com>
2024-12-20 16:41:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ chown -R www-data:www-data /rainloop/data
 find /rainloop -type d -exec chmod 755 {} \;
 find /rainloop -type f -exec chmod 644 {} \;
 
+
 # Call same entrypoint command as base image
 /usr/local/bin/apache2-foreground
